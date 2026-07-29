33c9280ff0d7659c07ca3e289ebb7ae002bd96ea
JosephNjorog <njorojoe11173@gmail.com>
2026-07-29 05:32:38 +0300

    Register ShortlistProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ import { TeamOpsProvider } from './context/TeamOpsContext'
 import { RosterProvider } from './context/RosterContext'
 import { MyOrgProvider } from './context/MyOrgContext'
 import { AdminFixturesProvider } from './context/AdminFixturesContext'
+import { ShortlistProvider } from './context/ShortlistContext'
 import { NotificationsProvider } from './context/NotificationsContext'
 import { CommentsProvider } from './context/CommentsContext'
 import { router } from './routes/router'
@@ -23,11 +24,13 @@ function App() {
                 <RosterProvider>
                   <MyOrgProvider>
                     <AdminFixturesProvider>
-                      <NotificationsProvider>
-                        <CommentsProvider>
-                          <RouterProvider router={router} />
-                        </CommentsProvider>
-                      </NotificationsProvider>
+                      <ShortlistProvider>
+                        <NotificationsProvider>
+                          <CommentsProvider>
+                            <RouterProvider router={router} />
+                          </CommentsProvider>
+                        </NotificationsProvider>
+                      </ShortlistProvider>
                     </AdminFixturesProvider>
                   </MyOrgProvider>
                 </RosterProvider>
